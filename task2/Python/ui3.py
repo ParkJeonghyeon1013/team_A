5b0b77f9e5039c20dc4b73d52a7182f2078475e0
parkjeonghyeon <pkys1020@naver.com>
2024-01-21 09:41:51 +0900

    ui2 ui3 success

--- task2/Python/ui3.py
+++ task2/Python/ui3.py
@@ -13,22 +13,37 @@ from task2.Qt import animal_farm_3_ui
 importlib.reload(animal_farm_3_ui)
 
 class UI_3(QtWidgets.QWidget, animal_farm_3_ui.Ui_Form):
-    def __init__(self, parent = None):
+    def __init__(self, fpath, a_sound, parent = None):
         super(UI_3, self).__init__(parent)
+
+        self.__fpath = fpath
+        self.__a_sound = a_sound
 
         self.setupUi(self)
         # self.set_label3()
 
+    @property
+    def fpath(self):
+        fpath = self.__fpath
+        return fpath
+
+    @property
+    def a_sound(self):
+        a_sound = self.__a_sound
+        return a_sound
+
+
+
     # textlabel의 내용을 사용자 인풋으로 가져가기.
-    def set_label3(self, a_name):
-        self.label__animal_name.setText(f'{a_name}.txt')
+    def set_label3(self):
+        self.label__animal_name.setText(f'{self.a_sound}.txt')
 
 
 
     # savefile 구현
-    def save_animal_file(self, fpath, a_sound):
-        with open(fpath, 'w', encoding='UTF8') as f:
-            f.write(a_sound)
+    def save_animal_file(self):
+        with open(self.fpath, 'w', encoding='UTF8') as f:
+            f.write(self.a_sound)
 
 
 
