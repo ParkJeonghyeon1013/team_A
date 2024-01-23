53d87ac58fb60766474148187f37ac1bd8ea2885
parkjeonghyeon <pkys1020@naver.com>
2024-01-23 18:06:32 +0900

    modifiy v1

--- task3/hi_json/hipy_json_4.py
+++ task3/hi_json/hipy_json_4.py
@@ -1,3 +1,5 @@
+# modify
+# insert, delete, modify, get
 json_data = {
     'project': {
         'name': 'mermaid',
@@ -25,15 +27,34 @@ json_data = {
     }
 }
 
-key_data = {'project': {'shot': {'EP0001': {'EP0001_0010': {'frange': None}}}}}
+# key_data = {'project': {'shot': {'EP0001': {'EP0001_0010': {'frange': None}}}}}
+#
+# json_modify(json_file, ['project', 'shot', 'EP0001', 'EP0001_0010', 'frange'], [1001, 1100])
+# json_modify(json_file, key_data, [1001, 1100])
+#
+# insert_key_data = {'project': {'shot': {'EP0005': {'EP0005_0050': {'frange': [1001, 1200]}}}}}
+#
+# json_insert(json_file, insert_key_data)
+#
+# delete_key_data = {'project': {'shot': {'EP0005': None}}}
+# json_delete(json_file, delete_key_data)
 
-# insert, delete, modify, get
-json_modify(json_file, ['project', 'shot', 'EP0001', 'EP0001_0010', 'frange'], [1001, 1100])
-json_modify(json_file, key_data, [1001, 1100])
+def json_modify(origin_data, v, word, path, location, chg_data):
+    for k, v in v.items():
+        path.append(k)
+        if word == k:
+            if path == location:
+                real_path = ''
+                for _ in path:
+                    real_path += f'[{_}]'
+                origin_data[real_path] = chg_data
+                return
 
-insert_key_data = {'project': {'shot': {'EP0005': {'EP0005_0050': {'frange': [1001, 1200]}}}}}
+        if isinstance(v, dict):
+            json_modify(origin_data, v, word, path, location, chg_data)
 
-json_insert(json_file, insert_key_data)
+path = []
+chg_data = [1001,1100]
+json_modify(json_data, json_data, 'frange', path, ['project', 'name', 'datetime', 'shot', 'EP0001', 'EP0001_0010', 'fps', 'frange'], chg_data)
 
-delete_key_data = {'project': {'shot': {'EP0005': None}}}
-json_delete(json_file, delete_key_data)
+print(json_data)
