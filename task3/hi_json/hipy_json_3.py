ec444fb5ee833742c32021beea03462d22d12ee5
kim.junhyuk <rlawns8688@gmail.com>
2024-01-26 10:01:21 +0900

    my get

--- task3/hi_json/hipy_json_3.py
+++ task3/hi_json/hipy_json_3.py
@@ -1,4 +1,4 @@
-json_data = {
+json_file = {
     'project': {
         'name': 'mermaid',
         'datetime': '2024-02-01',
@@ -26,14 +26,65 @@ json_data = {
 }
 
 key_data = {'project': {'shot': {'EP0001': {'EP0001_0010': {'frange': None}}}}}
+def json_modify(file, data_before, data_after):
+    return
 
-# insert, delete, modify, get
-json_modify(json_file, ['project', 'shot', 'EP0001', 'EP0001_0010', 'frange'], [1001, 1100])
-json_modify(json_file, key_data, [1001, 1100])
 
-insert_key_data = {'project': {'shot': {'EP0005': {'EP0005_0050': {'frange': [1001, 1200]}}}}}
 
-json_insert(json_file, insert_key_data)
+
+def json_insert(file, data_insert):
+    return
+
+
+
+
 
 delete_key_data = {'project': {'shot': {'EP0005': None}}}
-json_delete(json_file, delete_key_data)
+def json_delete(file, data_delete):
+    return
+
+
+# def json_get(file,data_get:str):
+#     if isinstance(file,dict):#인자로 넣은 파일이 디렉토리면 for문 실행
+#         for key,value in file.items():#인자의 key,value
+#             # print(file.values())
+#             if isinstance(value,list):
+#                 # print(type(value))
+#                 for i in range(len(value)):
+#                     # print('리스트안의 값',value[i],'값의 타입 ',type(value[i]))
+#                     if str(value[i]) == str(data_get):
+#                         print(value[i],'이거다',value,'안에있어요')
+#                         return value[i]
+#             else: json_get(value,data_get)
+
+import json
+
+def json_get(file, data_get):
+    if isinstance(file, dict):
+        for key, value in file.items():
+            if str(value) == str(data_get):
+                print(value, '이 값은', file, '안에 있어요')
+            result = json_get(value, data_get)
+            # if result is not None:
+            #     return result
+    #
+    # elif isinstance(file, list):
+    #     for idx in file:
+    #         if str(idx) == str(data_get):
+    #             print(idx, '이 값은', file, '안에 있어요')
+
+
+
+
+    # return None
+#
+print(json_get(json_file,'/home/rapa/nk'))
+
+# json_delete(json_file, delete_key_data)
+# # insert, delete, modify, get
+# json_modify(json_file, ['project', 'shot', 'EP0001', 'EP0001_0010', 'frange'], [1001, 1100])
+# json_modify(json_file, key_data, [1001, 1100])
+#
+# insert_key_data = {'project': {'shot': {'EP0005': {'EP0005_0050': {'frange': [1001, 1200]}}}}}
+#
+# json_insert(json_file, insert_key_data)
